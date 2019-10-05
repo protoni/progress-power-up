dc592cfc4926a6940b1dd9725befb1242963906e
Toni Kamula <protoni91@gmail.com>
2019-10-05 18:21:25 +0300

    Load a card.

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,6 +3,13 @@ console.log("Hello World!");
 // Init
 window.TrelloPowerUp.initialize({
   'card-badges': function(t, opts) {
-      return [];
+      return t.card('all')
+      .then(function(card) {
+          console.log(card);
+          return [{
+              text: card.idShort
+          }];
+      })
+      
   }
 })
