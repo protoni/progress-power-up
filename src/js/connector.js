b3b9bc3b7a1ed95885fdded82aaea813fab02048
Toni Kamula <protoni91@gmail.com>
2019-10-05 18:30:37 +0300

    Test progress bar div.

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,6 +3,19 @@ console.log("Hello World!");
 // Init
 window.TrelloPowerUp.initialize({
   'card-badges': function(t, opts) {
+        var container = document.createElement('div');
+        container.setAttribute('class', 'checklist-progress-bar');
+        var pbar = document.createElement('span');
+        //pbar.setAttribute('class', 'card-progressbar');
+        //pbar.setAttribute('value', listCompletion[0]);
+        //pbar.setAttribute('max', listCompletion[1]);
+        //pbar.setAttribute('style', 'width: 95%; height: 10px; background: green;');
+        pbar.setAttribute('class', 'checklist-progress-bar-current js-checklist-progress-bar');
+        pbar.setAttribute('style', 'width: '+progress+'%; background:#61BD4F;');
+        container.setAttribute('style', 'margin:0; height: 6px; background: #FFF6F6;');
+        container.appendChild(pbar);
+        cards[0].insertBefore(container, cards[i].childNodes[2]);
+      /*
       return t.card('all')
       .then(function(card) {
           console.log(card);
@@ -10,7 +23,7 @@ window.TrelloPowerUp.initialize({
               text: card.idShort
           }];
       })
-      
+      */
   }
 })
 
