6ebea4b99ab337d07b08ad4b8b3eff29d5e479c1
Toni Kamula <protoni91@gmail.com>
2019-10-05 18:38:49 +0300

    Fixed an array indexing bug.

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,7 +3,7 @@ console.log("Hello World!");
 // Init
 window.TrelloPowerUp.initialize({
   'card-badges': function(t, opts) {
-        let cards = document.getElementsByClassName('list-card-details');
+        let items = document.getElementsByClassName('list-card-details');
         let progress = 51;
         let container = document.createElement('div');
         container.setAttribute('class', 'checklist-progress-bar');
@@ -16,7 +16,7 @@ window.TrelloPowerUp.initialize({
         pbar.setAttribute('style', 'width: '+progress+'%; background:#61BD4F;');
         container.setAttribute('style', 'margin:0; height: 6px; background: #FFF6F6;');
         container.appendChild(pbar);
-        cards[0].insertBefore(container, cards[i].childNodes[2]);
+        items[0].insertBefore(container, items[0].childNodes[2]);
       /*
       return t.card('all')
       .then(function(card) {
@@ -28,30 +28,3 @@ window.TrelloPowerUp.initialize({
       */
   }
 })
-
-window.addEventListener ("load", Greasemonkey_main, false);
-function Greasemonkey_main () {
-  var cards = document.getElementsByClassName('list-card-details');
-  for (i = 0; i < cards.length; i++) {
-    var checklistBadges = cards[i].getElementsByClassName('icon-checklist');
-    if (checklistBadges.length > 0) {
-      if (checklistBadges[0].parentNode.childNodes.length > 0) {
-        var listCompletion = checklistBadges[0].parentNode.childNodes[1].innerHTML.split('/');
-        var progress = 100*listCompletion[0]/listCompletion[1];
-        if (progress == 100) return;
-        var container = document.createElement('div');
-        container.setAttribute('class', 'checklist-progress-bar');
-        var pbar = document.createElement('span');
-        //pbar.setAttribute('class', 'card-progressbar');
-        //pbar.setAttribute('value', listCompletion[0]);
-        //pbar.setAttribute('max', listCompletion[1]);
-        //pbar.setAttribute('style', 'width: 95%; height: 10px; background: green;');
-        pbar.setAttribute('class', 'checklist-progress-bar-current js-checklist-progress-bar');
-        pbar.setAttribute('style', 'width: '+progress+'%; background:#61BD4F;');
-        container.setAttribute('style', 'margin:0; height: 6px; background: #FFF6F6;');
-        container.appendChild(pbar);
-        cards[i].insertBefore(container, cards[i].childNodes[2]);
-      }
-    }
-  }
-}
